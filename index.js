be4dca8aa0f883b6ccebafc83a0a13d1e0121a78
Max Heyder <maxheyderart@gmail.com>
2017-03-27 09:54:32 +0200

    port changed

--- index.js
+++ index.js
@@ -4,9 +4,67 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-app.set('port', process.env.PORT || 3000 );
+app.set('port', process.env.PORT || 80 );
 
+var clients = [];
 
-server.listen(app.get('port'), function (){
+io.on("connection", function(socket)
+{
+	var currentUser;
+
+	socket.on("USER_CONNECT",function()
+	{
+		console.log("User connected");
+
+		for( var i = 0 ; i < clients.length ; i++ )
+		{
+			socket.emit("USER_CONNECTED", { name:clients[i].name , position:clients[i].position });
+			console.log("User: "+clients[i].name+ " is connected! There are now "+clients.length+" Users online!");
+		}
+
+	 });
+
+	socket.on("PLAY", function( data )
+	{
+			currentUser = 
+			{
+				name: data.name,
+				position: data.position
+			}
+			clients.push(currentUser);
+			console.log("User: "+currentUser.name+ " is connected! There are now "+clients.length+" Users online!");
+
+			socket.emit("PLAY", currentUser);
+			socket.broadcast.emit("USER_CONNECTED",currentUser);
+	});
+
+	socket.on("MOVE", function( data ) 
+	{
+			currentUser.position = data.position;
+
+			socket.emit("MOVE",currentUser);
+			socket.broadcast.emit("MOVE",currentUser);
+			console.log(currentUser.name+"move to "+currentUser.position);
+	});
+
+	socket.on("disconnect", function()
+	{
+		socket.broadcast.emit("USER_DISCONNECTED", currentUser );
+
+		for( var i = 0 ; i < clients.length ; i++ )
+		{
+			if( clients[i].name === currentUser.name)
+			{
+				var clientName = clients[i].name;
+				clients.splice(i,1);
+				console.log("User: "+clientName+ " disconnected! There are now "+clients.length+" Users online!");
+			}
+		};
+	});
+
+});
+
+server.listen(app.get('port'), function ()
+{
 	console.log("-----------SERVER IS RUNNING----------");
 });
