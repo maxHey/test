ff4e2b274c755a0b372c9196b0cd0b053caadfeb
Max Heyder <maxheyderart@gmail.com>
2017-03-27 14:30:59 +0200

    log

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 app.set('port', process.env.PORT || 3000 );
+//app.listen(8080);
 
 var clients = [];
 
