9eef1b1b63cf25a4757f2592f2660ab2a7a79bc4
Max Heyder <maxheyderart@gmail.com>
2017-03-27 14:50:13 +0200

    aa

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var https = require('https');
-var http = require('http');
 
-var server = http.createServer(app);
+var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 app.set('port', process.env.PORT || 80 );
-app.listen(80);
+//app.listen(80);
 //app.use(express.static(__dirname + "/../public"));
 //app.use(express.static('public'));
 //http.createServer(app).listen(80);
