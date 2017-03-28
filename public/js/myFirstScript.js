7bde9dfc27d90012ba0b0e604198b132d3679883
Max Heyder <maxheyderart@gmail.com>
2017-03-28 13:21:10 +0200

    threeJS example code cleanup

--- public/js/myFirstScript.js
+++ public/js/myFirstScript.js
@@ -12,20 +12,21 @@ animate();
 
 function init() {
 
+    //*************** START
     container = document.createElement( 'div' );
     document.body.appendChild( container );
 
+    //*************** CAMERA
     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
     camera.position.set( 0, 200, 800 );
 
+    //*************** SCENE
     scene = new THREE.Scene();
 
-    // Grid
-
+    //*************** GRID
     var line_material = new THREE.LineBasicMaterial( { color: 0x303030 } ),
         geometry = new THREE.Geometry(),
         floor = -75, step = 25;
-
     for ( var i = 0; i <= 40; i ++ ) {
 
         geometry.vertices.push( new THREE.Vector3( - 500, floor, i * step - 500 ) );
@@ -35,7 +36,6 @@ function init() {
         geometry.vertices.push( new THREE.Vector3( i * step - 500, floor,  500 ) );
 
     }
-
     var line = new THREE.LineSegments( geometry, line_material );
     scene.add( line );
 
@@ -49,7 +49,6 @@ function init() {
     materials.push( new THREE.MeshPhongMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 30, shading: THREE.FlatShading } ) );
     materials.push( new THREE.MeshNormalMaterial( ) );
     materials.push( new THREE.MeshBasicMaterial( { color: 0xffaa00, transparent: true, blending: THREE.AdditiveBlending } ) );
-    //materials.push( new THREE.MeshBasicMaterial( { color: 0xff0000, blending: THREE.SubtractiveBlending } ) );
 
     materials.push( new THREE.MeshLambertMaterial( { color: 0xdddddd, shading: THREE.SmoothShading } ) );
     materials.push( new THREE.MeshPhongMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 30, shading: THREE.SmoothShading, map: texture, transparent: true } ) );
@@ -63,74 +62,52 @@ function init() {
 
     materials.push( new THREE.MeshBasicMaterial( { map: texture, transparent: true } ) );
 
-    // Spheres geometry
-
+    //*************** GEOMETRY
+    //** BASIC SPHERE MESH
     var geometry = new THREE.SphereGeometry( 70, 32, 16 );
-
-    for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
-
-        var face = geometry.faces[ i ];
-        face.materialIndex = Math.floor( Math.random() * materials.length );
-
-    }
-
-    geometry.sortFacesByMaterialIndex();
-
     objects = [];
-
+    //** ADD MATERIALS TO EACH SPHERE
     for ( var i = 0, l = materials.length; i < l; i ++ ) {
 
         addMesh( geometry, materials[ i ] );
 
     }
-
-    addMesh( geometry, new THREE.MultiMaterial( materials ) );
-
+    //** PARTICLE LIGHT
     particleLight = new THREE.Mesh( new THREE.SphereGeometry( 4, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0xffffff } ) );
     scene.add( particleLight );
 
-    // Lights
-
+    //*************** LIGHTS
+    //** AMBIENTLIGHT
     scene.add( new THREE.AmbientLight( 0x111111 ) );
-
+    //** DIRECTIONAL
     var directionalLight = new THREE.DirectionalLight( /*Math.random() * */ 0xffffff, 0.125 );
-
     directionalLight.position.x = Math.random() - 0.5;
     directionalLight.position.y = Math.random() - 0.5;
     directionalLight.position.z = Math.random() - 0.5;
-
     directionalLight.position.normalize();
-
     scene.add( directionalLight );
-
+    //** POINTLIGHT
     var pointLight = new THREE.PointLight( 0xffffff, 1 );
     particleLight.add( pointLight );
-
     //
-
+    //*************** ADD the RENDERER
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
+    //*************** CONTAINER
     container.appendChild( renderer.domElement );
-
     //
-
     stats = new Stats();
     container.appendChild( stats.dom );
-
     //
-
     window.addEventListener( 'resize', onWindowResize, false );
-
 }
 
 function addMesh( geometry, material ) {
-
     var mesh = new THREE.Mesh( geometry, material );
 
     mesh.position.x = ( objects.length % 4 ) * 200 - 400;
     mesh.position.z = Math.floor( objects.length / 4 ) * 200 - 200;
-
     mesh.rotation.x = Math.random() * 200 - 100;
     mesh.rotation.y = Math.random() * 200 - 100;
     mesh.rotation.z = Math.random() * 200 - 100;
@@ -142,16 +119,13 @@ function addMesh( geometry, material ) {
 }
 
 function onWindowResize() {
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
 
 function generateTexture() {
-
     var canvas = document.createElement( 'canvas' );
     canvas.width = 256;
     canvas.height = 256;
@@ -162,7 +136,6 @@ function generateTexture() {
     var x = 0, y = 0;
 
     for ( var i = 0, j = 0, l = image.data.length; i < l; i += 4, j ++ ) {
-
         x = j % 256;
         y = x == 0 ? y + 1 : y;
 
@@ -170,13 +143,11 @@ function generateTexture() {
         image.data[ i + 1 ] = 255;
         image.data[ i + 2 ] = 255;
         image.data[ i + 3 ] = Math.floor( x ^ y );
-
     }
 
     context.putImageData( image, 0, 0 );
 
     return canvas;
-
 }
 
 //
@@ -187,33 +158,30 @@ function animate() {
 
     render();
     stats.update();
-
 }
 
 function render() {
 
     var timer = 0.0001 * Date.now();
-
-    camera.position.x = Math.cos( timer ) * 1000;
-    camera.position.z = Math.sin( timer ) * 1000;
+    //camera.position.x = Math.cos( timer ) * 1000;
+    //camera.position.z = Math.sin( timer ) * 1000;
 
     camera.lookAt( scene.position );
 
+    /*
     for ( var i = 0, l = objects.length; i < l; i ++ ) {
-
         var object = objects[ i ];
-
         object.rotation.x += 0.01;
         object.rotation.y += 0.005;
-
     }
+    */
 
     materials[ materials.length - 2 ].emissive.setHSL( 0.54, 1, 0.35 * ( 0.5 + 0.5 * Math.sin( 35 * timer ) ) );
     materials[ materials.length - 3 ].emissive.setHSL( 0.04, 1, 0.35 * ( 0.5 + 0.5 * Math.cos( 35 * timer ) ) );
 
-    particleLight.position.x = Math.sin( timer * 7 ) * 300;
-    particleLight.position.y = Math.cos( timer * 5 ) * 400;
-    particleLight.position.z = Math.cos( timer * 3 ) * 300;
+    //particleLight.position.x = Math.sin( timer * 7 ) * 300;
+    //particleLight.position.y = Math.cos( timer * 5 ) * 400;
+    //particleLight.position.z = Math.cos( timer * 3 ) * 300;
 
     renderer.render( scene, camera );
 
