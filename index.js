370baa666a84aa4eb31a5738567a15b107487564
Max Heyder <maxheyderart@gmail.com>
2017-03-29 09:13:39 +0200

    PC version

--- index.js
+++ index.js
@@ -78,8 +78,8 @@ io.on("connection", function(socket)
 	socket.on("disconnect", function()
 	{
 		console.log("a user disconnected!");
-		socket.broadcast.emit("USER_DISCONNECTED", currentUser );
-
+		//socket.broadcast.emit("USER_DISCONNECTED", currentUser );
+		/*
 		for( var i = 0 ; i < clients.length ; i++ )
 		{
 			if( clients[i].name === currentUser.name)
@@ -88,7 +88,8 @@ io.on("connection", function(socket)
 				clients.splice(i,1);
 				//console.log("User: "+clientName+ " disconnected! There are now "+clients.length+" Users online!");
 			}
-		};
+		}
+		*/
 	});
 
 	function mainLoop()
